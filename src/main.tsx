c21114091dcb4abd32fff9a5d34b33da18ec87e9
Lisa0904 <l.taljanovic@web.de>
2026-02-02 12:44:35 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
-import './Snake.css';
+import './snake.css';
 import './Flashcards.css';
 import './memory.css';
 
